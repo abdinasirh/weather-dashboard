c236de4e25ec012fcaf40cea192433bec30d6759
Abdinasir Hassan <abdinasirduud20@gmail.com>
2021-11-20 13:56:52 -0600

    added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,38 @@
 var apiKey = "a2c9303f1d7e4e010eb8c74634ac29bf";
+
+
+
+var inputValue = $("#input-value");
+var searchBtn = $("#search-btn");
+var cityNameEl = $("#city-name");
+var currentDateEl = $("#current-date");
+var weatherIconEl = $("#weather-icon");
+var tempEl = $("#temperature");
+var humidityEl = $("#humidity");
+var WindSpeedEl = $("#wind-speed");
+var uvIndexEl = $("#UV-index");
+var cardRowEl = $("#card-row");
+
+
+searchBtn.on("click", function(){
+    // preventDefault();
+    if (inputValue.val() === "") {
+        alert("You must enter a city");
+        return;
+    }
+    console.log("you clicked this button")
+    // getWeather(searchInput.val());
+});
+
+
+
+
+
+
+
+
+
+
 function displayWeather(){
     var url = "https://api.openweathermap.org/data/2.5/weather?q=saint%20cloud&appid=a2c9303f1d7e4e010eb8c74634ac29bf";
 
