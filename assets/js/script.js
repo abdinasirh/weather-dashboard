20ea95e07c14c5c3f2fdfdb8a766c2b730ffd356
Abdinasir Hassan <abdinasirduud20@gmail.com>
2021-11-21 17:58:30 -0600

    added a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,55 +2,57 @@ var apiKey = "a2c9303f1d7e4e010eb8c74634ac29bf";
 
 
 
-var inputValue = $("#input-value");
-var searchBtn = $("#search-btn");
-var cityNameEl = $("#city-name");
-var currentDateEl = $("#current-date");
-var weatherIconEl = $("#weather-icon");
-var tempEl = $("#temperature");
-var humidityEl = $("#humidity");
-var WindSpeedEl = $("#wind-speed");
-var uvIndexEl = $("#UV-index");
-var cardRowEl = $("#card-row");
-
-
-searchBtn.on("click", function(){
-    // preventDefault();
-    if (inputValue.val() === "") {
-        alert("You must enter a city");
-        return;
-    }
-    console.log("you clicked this button")
-    // getWeather(searchInput.val());
-});
-
-
-
-
-
+// var inputValue = $("#input-value");
+var searchBtn = document.querySelector("#search-btn");
+var cityNameEl = document.querySelector("#city-name");
+var currentDateEl = document.querySelector("#current-date");
+var weatherIconEl = document.querySelector("#weather-icon");
+var tempEl = document.querySelector("#temperature");
+var humidityEl = document.querySelector("#humidity");
+var WindSpeedEl = document.querySelector("#wind-speed");
+var uvIndexEl = document.querySelector("#UV-index");
+var cardRowEl = document.querySelector("#card-row");
 
 
 
 
 
 function displayWeather(){
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=saint%20cloud&appid=a2c9303f1d7e4e010eb8c74634ac29bf";
+    let inputValue=$("#input-value").val()  
+      console.log(inputValue)
+    
+    
+        var apir = "https://api.openweathermap.org/data/2.5/weather?q=" +inputValue+ "&appid=" + apiKey;
+        console.log(apir)
+        
+        fetch(apir).then(function(response){
+            if (response.ok){
+                return response.json()
+            } 
+        })
+        .then(function(data){
+            console.log("data ", data)
+            // searchedResult(data)
+        })
+    }
 
-    fetch(url).then(function(resp) { return resp.json() }) // Convert data to json
-    .then(function(data) {
-      console.log(data);
-    })
+    // function searchedResult(data){
 
-}
-
-displayWeather();
+    // }
+    
+        
 
 
+    searchBtn.addEventListener("click",displayWeather )
 
 
 
-// fetch(apiKey).then(function(response) {
-//     // request was successful
-//     if (response.ok) {
-//       response.json().then(function(data) {
-//         displayIssues(data);
+
+
+
+
+
+
+
+
+
