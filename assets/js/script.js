46f1f5f0ea8b96b06f587cbf960890293e9a8fce
Abdinasir Hassan <abdinasirduud20@gmail.com>
2021-11-22 16:51:58 -0600

    added html and js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+
+
 var apiKey = "a2c9303f1d7e4e010eb8c74634ac29bf";
 
 
 
-// var inputValue = $("#input-value");
+// var inputValue = document.querySelector("#input-value");
 var searchBtn = document.querySelector("#search-btn");
 var cityNameEl = document.querySelector("#city-name");
 var currentDateEl = document.querySelector("#current-date");
@@ -12,19 +14,22 @@ var humidityEl = document.querySelector("#humidity");
 var WindSpeedEl = document.querySelector("#wind-speed");
 var uvIndexEl = document.querySelector("#UV-index");
 var cardRowEl = document.querySelector("#card-row");
-var currentWeather = document.querySelector("#current-weather-container");
+var currentWeather = document.querySelector("#current-weather");
 var currentCity = document.querySelector("#current-city");
 
 
 
 
 
-function gitCityWeather(){
+
+
+function getCityWeather(){
     let inputValue=$("#input-value").val()  
       console.log(inputValue)
     
     
-        var apir = "https://api.openweathermap.org/data/2.5/weather?q=" +inputValue+ "&appid=" + apiKey;
+        // var apir = "https://api.openweathermap.org/data/2.5/weather?q=minneapolis&units=imperial&appid=" + apiKey;
+        var apir = "https://api.openweathermap.org/data/2.5/weather?q=" +inputValue+ "&units=imperial&appid=" + apiKey;
         console.log(apir)
         
         fetch(apir).then(function(response){
@@ -32,9 +37,31 @@ function gitCityWeather(){
                 return response.json()
             } 
         })
+
         .then(function(data){
-            console.log("data ", data)
-            displayWeather(data)
+            // console.log("data ", data)
+
+            // var h1El = document.createElement("h3");
+            // h1El.textContent=data.name;
+            // currentCity.appendChild(h1El);
+            currentCity.textContent=data.name;
+
+            var currentDate = moment().format('MMMM Do YYYY');
+            currentDateEl.textContent = currentDate;
+
+            var iconSRC = ` http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            weatherIconEl.setAttribute("src", iconSRC);
+
+            tempEl.textContent= "temp: " + data.main.temp +" f"
+
+
+
+
+
+
+
+            fiveDay(inputValue)
+            // displayWeather(data)
 
             
         })
@@ -43,20 +70,55 @@ function gitCityWeather(){
       
     }
 
-    function displayWeather(data){
+    function fiveDay(cityInput){
 
-        currentWeather.innerHTML=""
-        currentCity.innerHTML = response.data.name;
+        var fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial&appid=" + apiKey;
+
+        fetch(fiveDayApi).then(function(response){
+            if (response.ok){
+                return response.json()
+            } 
+        })
+
+        .then(function(data){
+            console.log("data ", data)
+
+            for (let i = 0; i < 5; i++) {
+                console.log(data.list[i].main.temp);
+
+                var card = document.createElement("div");
+                card.setAttribute("class", "card");
+                //card body
+                //give it content
+                //append the content to the card body
+                //apend the card body to the card 
+
+                //append the card to your 5 day forecast html element
+                
+            }
 
 
-
-
+            
+        })
+        
     }
+    // getCityWeather();
+
+//     function displayWeather(data){
+
+//         currentWeather.innerHTML=""
+//         currentCity.innerHTML = response.name;
+//         currentWeather.appendChild(currentCity);
+
+
+
+
+//     }
     
         
 
 
-    searchBtn.addEventListener("click",displayWeather )
+    searchBtn.addEventListener("click",getCityWeather )
 
 
 
